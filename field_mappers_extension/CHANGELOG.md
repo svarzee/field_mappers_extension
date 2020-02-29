c229b9ec0cd9c4dfedacd85ad06a455c9fcb8b17
svarzee <svarzee@gmail.com>
2020-02-29 17:52:22 +0100

    Fix generics

--- field_mappers_extension/CHANGELOG.md
+++ field_mappers_extension/CHANGELOG.md
@@ -1,2 +1,5 @@
+## 0.2.0
+* Fix generics
+
 ## 0.1.0
 * Initialize package
--- field_mappers_extension/lib/src/field_mappers_generator.dart
+++ field_mappers_extension/lib/src/field_mappers_generator.dart
@@ -15,7 +15,7 @@ class FieldMappersGenerator extends ClassExtensionGenerator<FieldMappers> {
 
   String _generateFieldMapper(ClassElement clazz, FieldElement field) {
     final fieldMapperName = 'map${field.name[0].toUpperCase()}${field.name.substring(1)}';
-    return '${clazz.name} ${fieldMapperName}(${field.type.name} Function(${field.type.name}) map) => ${clazz.name}${_generateCtor(clazz, field)};';
+    return '${clazz.name} ${fieldMapperName}(${field.type.toString()} Function(${field.type.toString()}) map) => ${clazz.name}${_generateCtor(clazz, field)};';
   }
 
   String _generateCtor(ClassElement clazz, FieldElement field) {
@@ -27,6 +27,8 @@ class FieldMappersGenerator extends ClassExtensionGenerator<FieldMappers> {
   }
 
   String _generateCtorArg(ClassElement clazz, FieldElement field, ParameterElement param) {
-    return field.name == param.name ? 'map((this as ${clazz.name}).${param.name})' : '(this as ${clazz.name}).${param.name}';
+    return field.name == param.name
+        ? 'map((this as ${clazz.name}).${param.name})'
+        : '(this as ${clazz.name}).${param.name}';
   }
 }
